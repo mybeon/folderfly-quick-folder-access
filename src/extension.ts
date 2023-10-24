3eb6f0f410f3e566738d8fe7c6ab8522642754bc
hichamelb <hichamelb54@gmail.com>
2023-10-24 23:10:21 +0100

    Trigger an error condition when there are no remaining directories to recursively traverse

--- src/extension.ts
+++ src/extension.ts
@@ -4,26 +4,34 @@ import * as fs from "fs/promises";
 import * as path from "path";
 import * as vscode from "vscode";
 
-type items = vscode.QuickPickItem & { isProject: boolean; path: string };
+type Items = vscode.QuickPickItem & { isProject: boolean; path: string };
 
-async function getFolders(folderPath: string): Promise<items[]> {
-    const folders = await fs.readdir(path.join(folderPath));
-    const folderContent = await Promise.all(
-        folders.map(folderName => fs.readdir(path.join(folderPath, folderName)))
-    );
+async function getFolders(folderPath: string): Promise<Items[]> {
+    try {
+        const folders = await fs.readdir(path.join(folderPath));
+        const folderContent = await Promise.all(
+            folders.map(folderName => fs.readdir(path.join(folderPath, folderName)))
+        );
 
-    return folders.map((folder, index) => {
-        const isProject = folderContent[index].includes(".git");
-        return {
-            label: folder,
-            iconPath: isProject ? new vscode.ThemeIcon("source-control") : vscode.ThemeIcon.Folder,
-            path: path.join(folderPath, folder),
-            isProject,
-        };
-    });
+        return folders.map((folder, index) => {
+            const isProject = folderContent[index].includes(".git");
+            return {
+                label: folder,
+                iconPath: isProject
+                    ? new vscode.ThemeIcon("source-control")
+                    : vscode.ThemeIcon.Folder,
+                path: path.join(folderPath, folder),
+                isProject,
+            };
+        });
+    } catch (e) {
+        return [];
+    }
 }
 
-async function showFolders(folders: items[]) {
+async function showFolders(folders: Items[]) {
+    if (folders.length === 0) return vscode.window.showErrorMessage("Can't go further !");
+
     const folder = await vscode.window.showQuickPick(folders, {
         placeHolder: "choose your folder",
     });
@@ -31,12 +39,14 @@ async function showFolders(folders: items[]) {
     if (typeof folder === "undefined") return;
 
     // vscode.workspace.updateWorkspaceFolders(0, null, { uri: vscode.Uri.file(folder.path) });
+
     if (folder.isProject) {
         vscode.commands.executeCommand("vscode.openFolder", vscode.Uri.file(folder.path), {
             forceReuseWindow: true,
         });
     } else {
         const childrenFolders = await getFolders(folder.path);
+
         showFolders(childrenFolders);
     }
 }
